bd6e2dd34963821cf47f39a6f6438d7fd7b050c8
Athisankar-ssk <sankarathi49@gmail.com>
2026-02-05 21:13:01 +0530

    add booking detail in meeting hall

--- frontend/src/pages/AdminBookingDetails.jsx
+++ frontend/src/pages/AdminBookingDetails.jsx
@@ -35,7 +35,7 @@ function AdminBookingDetails() {
       setLoading(true);
       const token = localStorage.getItem("adminToken");
       
-      // Fetch playground bookings
+      // Fetch playground booking
       const playgroundRes = await axios.get(
         "http://localhost:5000/api/playground/admin/all-bookings",
         { headers: { Authorization: `Bearer ${token}` } }

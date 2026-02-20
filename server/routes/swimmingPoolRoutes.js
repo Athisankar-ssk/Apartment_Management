1df26b09bc7c917854d16e12c3d784b906c65645
Athisankar-ssk <sankarathi49@gmail.com>
2026-02-20 19:27:25 +0530

    change

--- server/routes/swimmingPoolRoutes.js
+++ server/routes/swimmingPoolRoutes.js
@@ -6,7 +6,7 @@ import { canCancelBefore } from '../utils/cancellation.js';
 
 const router = express.Router();
 
-// Mddleware to verify user token
+// Middleware to verify user token
 const authenticateUser = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(' ')[1];

18b7d3b2a738965a859b23bf6512b9d8d38c8a28
Athisankar-ssk <sankarathi49@gmail.com>
2026-02-24 18:00:19 +0530

    change adminBooking

--- frontend/src/pages/AdminBookingDetails.jsx
+++ frontend/src/pages/AdminBookingDetails.jsx
@@ -406,7 +406,7 @@ function AdminBookingDetails() {
             {loading ? (
               <p style={{ color: "#64748b" }}>Loading bookings..</p>
             ) : activeTab === "playground" ? (
-              // Playground Bookings Tabl
+              // Playground Bookings Table
               filteredPlaygroundBookings.length === 0 ? (
                 <p style={{ color: "#64748b" }}>No playground bookings found</p>
               ) : (
